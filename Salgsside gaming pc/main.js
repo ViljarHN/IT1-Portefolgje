eda6dade660f068b90345acaf49471d6335f4e2e
Viljar Hole Nestande <vilnesto.hole@gmail.com>
2022-11-22 13:40:41 +0100

    salgside

---
+++ Salgsside gaming pc/main.js
@@ -0,0 +1,22 @@
+/*
+const asus_logo = document.getElementById('asus-logo')
+const gainward_logo = document.getElementById('gainward-logo')
+const intel_logo = document.getElementById('intel-logo')
+const kingtson_logo = document.getElementById('kingston-logo')
+const msi_logo = document.getElementById('msi-logo')
+
+window.addEventListener('scroll', function() {
+    let windowY = window.scrollY
+    console.log(windowY)
+    asus_logo.style.top = windowY
+    asus_logo.style.left = 
+    gainward_logo.style.top
+    gainward_logo.style.left
+    intel_logo.style.top
+    intel_logo.style.left
+    kingtson_logo.style.top
+    kingtson_logo.style.left
+    msi_logo.style.top
+    msi_logo.style.left
+})
+*/
