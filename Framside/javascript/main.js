421294a4dbab930532656ae143825341c6c96a43
Viljar Hole Nestande <vilnesto.hole@gmail.com>
2022-11-24 13:52:35 +0100

    new design

--- Framside/javascript/main.js
+++ Framside/javascript/main.js
@@ -7,7 +7,17 @@ function close_help() {
     const helpModal = document.getElementById('helpModal')
     helpModal.style.display = 'none'
 }
-
+/*
 function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px'
+}
+*/
+
+function add_class(id) {
+    const element = document.querySelectorAll('section')
+    for (let i = 0; i < element.length; i++) {
+        element[i].classList.remove('active')
+    }
+    const addToClass = document.getElementById(id+'-section')
+    addToClass.classList.add('active')
 }
