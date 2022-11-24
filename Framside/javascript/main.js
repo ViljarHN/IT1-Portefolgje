6564eff83765dae5fccd77ec6467aa4ff2db3455
Viljar Hole Nestande <vilnesto.hole@gmail.com>
2022-11-24 19:50:21 +0100

    automatic navigationbar

--- Framside/javascript/main.js
+++ Framside/javascript/main.js
@@ -1,3 +1,4 @@
+/*
 function show_help() {
     const helpModal = document.getElementById('helpModal')
     helpModal.style.display = 'block'
@@ -7,17 +8,37 @@ function close_help() {
     const helpModal = document.getElementById('helpModal')
     helpModal.style.display = 'none'
 }
-/*
+*/
+
 function resizeIframe(obj) {
     obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px'
 }
-*/
 
-function add_class(id) {
-    const element = document.querySelectorAll('section')
-    for (let i = 0; i < element.length; i++) {
-        element[i].classList.remove('active')
-    }
-    const addToClass = document.getElementById(id+'-section')
-    addToClass.classList.add('active')
+const navLink = document.querySelectorAll('.h-navbar li a')
+const section = document.querySelectorAll('section')
+
+window.onscroll = () => {
+    const bottomOfWebpage = document.body.scrollHeight - window.innerHeight
+    let current = ""
+    section.forEach(section => {
+        if (Math.round(window.scrollY) >=  section.offsetTop - 1) {
+            current = section.getAttribute('id')
+        } 
+        if (Math.round(window.scrollY) === bottomOfWebpage) {
+            current = section.getAttribute('id')
+        }
+    });
+    navLink.forEach(link => {
+        link.classList.remove('active')
+        if (link.classList.contains(current)) {
+            link.classList.add('active')
+        }
+    });
 }
+/*
+function ScrollToSection(id) {
+    const currentSection = document.getElementById(id+'-selector')
+    currentSection.scrollIntoView();
+    //HTML: onmouseup="ScrollToSection(this.id);"
+}
+*/
