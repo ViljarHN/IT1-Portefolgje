cedebea1de31ea3145dfde0181194cad73421b3a
Viljar Hole Nestande <vilnesto.hole@gmail.com>
2022-11-24 11:05:04 +0100

    iframe-resize function

--- Framside/javascript/main.js
+++ Framside/javascript/main.js
@@ -7,3 +7,7 @@ function close_help() {
     const helpModal = document.getElementById('helpModal')
     helpModal.style.display = 'none'
 }
+
+function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px'
+}
