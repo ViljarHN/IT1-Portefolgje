46df3081e15efde0622498d4cba99f410877f54a
Viljar Hole Nestande <vilnesto.hole@gmail.com>
2022-11-26 13:09:56 +0100

    programming language skill circle animation

--- Framside/javascript/main.js
+++ Framside/javascript/main.js
@@ -11,30 +11,80 @@ function close_help() {
 */
 
 function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px'
+    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
 }
 
-const navLink = document.querySelectorAll('.h-navbar li a')
-const section = document.querySelectorAll('section')
+const navLink = document.querySelectorAll('.h-navbar li a');
+const section = document.querySelectorAll('section');
+let sectionArray = [];
 
 window.onscroll = () => {
-    const bottomOfWebpage = document.body.scrollHeight - window.innerHeight
-    let current = ""
+    let current = "";
     section.forEach(section => {
-        if (Math.round(window.scrollY) >=  section.offsetTop - 1) {
-            current = section.getAttribute('id')
-        } 
-        if (Math.round(window.scrollY) === bottomOfWebpage) {
-            current = section.getAttribute('id')
+        if (window.scrollY + window.innerHeight/2 >= section.offsetTop) {
+            current = section.getAttribute('id');
         }
     });
     navLink.forEach(link => {
-        link.classList.remove('active')
+        link.classList.remove('active');
         if (link.classList.contains(current)) {
-            link.classList.add('active')
+            link.classList.add('active');
         }
     });
 }
+
+const showSkill = (id) => {
+    let progressValue = 0;
+    let skillValue = 0;
+    let skillName = '';
+    const element = document.getElementById(id);
+    const element_value = document.getElementById(id+'-skill');
+    element.style.scale = '1.2';
+    switch (id) {
+        case 'html-progress':
+            skillValue = 80;
+            break;
+        case 'css-progress':
+            skillValue = 70;
+            break;
+        case 'js-progress':
+            skillValue = 90;
+            break;
+    }
+    let progress = setInterval(() => {
+        progressValue++;
+        element_value.innerHTML = `${progressValue}%`
+        element.style.background = `conic-gradient(red ${progressValue * 3.6}deg, yellow ${progressValue * 3.6}deg)`;
+        if (progressValue >= skillValue) {
+            clearInterval(progress);
+        }
+    });
+    setTimeout(() => {
+        switch (id) {
+            case 'html-progress':
+                skillName = 'HTML';
+                break;
+            case 'css-progress':
+                skillName = 'CSS';
+                break;
+            case 'js-progress':
+                skillName = 'JS';
+                break;
+        }
+        element.style.scale = '1';
+        element_value.innerHTML = skillName;
+        element.style.background = 'yellow'
+    }, 3000)
+}
+
+/*
+window.onload = () => {
+    section.forEach(section => {
+        sectionArray.push((section.offsetTop + section.offsetHeight/2) - section.offsetHeight, section.offsetTop + section.offsetHeight/2)
+        console.log(section.offsetTop, section.offsetHeight, section.offsetHeight/2, section.offsetTop + section.offsetHeight/2)
+    })
+}
+*/
 /*
 function ScrollToSection(id) {
     const currentSection = document.getElementById(id+'-selector')
