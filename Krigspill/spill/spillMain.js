cee4fa27b61cb5490d6d4b7109f66a146d15f1ef
Viljar Hole Nestande <vilnesto.hole@gmail.com>
2023-01-05 09:56:04 +0100

    while-loop

--- Krigspill/spill/spillMain.js
+++ Krigspill/spill/spillMain.js
@@ -1,2 +1,13 @@
 let lagA = 50
 let lagB = 60
+
+while (lagA >= 0 || lagB >= 0) {
+    const randNum = Math.random()
+    if (randNum >= 0.5) {
+        lagA--;
+        console.log(lagA)
+    } else {
+        lagB--;
+        console.log(lagB)
+    }
+}
